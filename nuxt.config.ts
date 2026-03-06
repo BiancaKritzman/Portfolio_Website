b535271940d87a9f06ba3847a76cc795a8bd43ae
BiancaKritzman <biancakritzman@gmail.com>
2026-03-06 19:44:09 +0200

    add static to nuxt.config.ts for deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,       
+  ssr: false,
+  nitro: {
+    preset: 'static'
+  },       
   app: {
     baseURL: '/',  
   },
